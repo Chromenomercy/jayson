7eaada8dda9008f8383baf8b4f775af2b291dd82
bobnomercy <jack.parsons.uk@gmail.com>
2015-06-22 17:45:32 +0100

    Call to Interface

--- Jayson/Program.cs
+++ Jayson/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Jayson.Interface;
 
 namespace Jayson
 {
     class Program
-    {
+	{
+		Object interface_Jayson = new Interface();
+
         static void Main(string[] args)
         {
+			while (true) 
+			{
+				interface_Jayson.Read ();
+			}
         }
     }
 }
