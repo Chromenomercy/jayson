f55261bc15b439bfd0f55e7dcfabef0c2f8feaf4
Chromadude <PeterSearby8@googlemail.com>
2015-06-22 18:19:40 +0100

    basic reader

--- Jayson/Interface.cs
+++ Jayson/Interface.cs
@@ -8,5 +8,30 @@ namespace Jayson
 {
     class Interface
     {
+        public Tuple<string, Int16, string> Read()
+        {
+            String raw_input;
+            String[] input;
+            String name;
+            Int16 type;
+            String value;
+
+            raw_input = Console.ReadLine();
+            input = raw_input.Split(':');
+            try
+            {
+                name = input[0];
+                type = Convert.ToInt16(input[1]);
+                value = input[2];
+            }
+            catch (Exception e)
+            {
+                return new Tuple<string, Int16, string>("jayson_error", 1, e.ToString());
+            }
+            
+            Tuple<string, Int16, string> data = new Tuple<string, Int16, string>(name, type, value);
+            return data;
+        }
+
     }
 }
--- Jayson/Program.cs
+++ Jayson/Program.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Jayson.Interface;
 
 namespace Jayson
 {
     class Program
 	{
-		Object interface_Jayson = new Interface();
-
         static void Main(string[] args)
         {
+		    Interface interface_jayson = new Interface();
 			while (true) 
 			{
-				interface_Jayson.Read ();
+				Tuple<string, Int16, string>data = interface_jayson.Read ();
+                Console.WriteLine(data);
 			}
         }
     }
