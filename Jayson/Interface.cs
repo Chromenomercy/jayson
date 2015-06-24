d7f166efc310c98e947dc3c9e1639a2d4ea7709c
Chromadude <PeterSearby8@googlemail.com>
2015-06-24 07:39:14 +0100

    organising stuff

--- Jayson/Interface.cs
+++ Jayson/Interface.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 
 namespace Jayson
 {
     public class Interface
     {
+        SpeechSynthesizer synth;
+        SpeechRecognitionEngine sre;
+        public Interface()
+        {
+            synth = new SpeechSynthesizer();
+            sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            Choices words = new Choices();
+            words.Add(new string[] { "red", "green", "jason", "blue", "peter", "jack"});
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(words);
+
+            // Create the Grammar instance.
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_Speechrecognised);
+            sre.SetInputToDefaultAudioDevice();
+            synth.SetOutputToDefaultAudioDevice();
+        }
+
         public Tuple<string, Int16, string> Read()
         {
             String raw_input;
@@ -31,6 +54,11 @@ namespace Jayson
             
             Tuple<string, Int16, string> data = new Tuple<string, Int16, string>(name, type, value);
             return data;
+        }
+
+        private void sre_Speechrecognised(object sender, SpeechRecognizedEventArgs e)
+        {
+            synth.Speak("You said: " + e.Result.Text);
         }
 
     }
--- Jayson/Program.cs
+++ Jayson/Program.cs
@@ -2,40 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Speech.Recognition;
-using System.Speech.Synthesis;
 using System.Diagnostics;
 
 namespace Jayson
 {
     class Program
     {
-        private static SpeechSynthesizer synth = new SpeechSynthesizer();
         static void Main(string[] args)
         {
-            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
-            synth.SelectVoiceByHints(VoiceGender.Female);
-            Choices words = new Choices();
-            words.Add(new string[] { "red", "green", "jason", "blue", "peter is the most awesome person ever"});
-
-            GrammarBuilder gb = new GrammarBuilder();
-            gb.Append(words);
-
-            // Create the Grammar instance.
-            Grammar g = new Grammar(gb);
-            sre.LoadGrammar(g);
-            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_Speechrecognised);
-            sre.SetInputToDefaultAudioDevice();
-            synth.SetOutputToDefaultAudioDevice();
-            while (true)
-            {
-                sre.Recognize();
-            }
-        }
-
-        private static void sre_Speechrecognised(object sender, SpeechRecognizedEventArgs e)
-        {
-            synth.Speak("Speech recognized: " + e.Result.Text);
+            Interface jayInterface = new Interface();
         }
 
     }
