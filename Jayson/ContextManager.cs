1919de5ee1c9e931a0a8f73fe4f1dd1b86554ffe
Chromadude <PeterSearby8@googlemail.com>
2015-09-07 21:16:05 +0100

    adding update method to ContextManager

--- Jayson/ContextManager.cs
+++ Jayson/ContextManager.cs
@@ -8,9 +8,35 @@ namespace Jayson
 {
     class ContextManager
     {
+        Dictionary<string, byte> wordsInContext;
         public ContextManager()
         {
             Dictionary<string, byte> wordsInContext = new Dictionary<string, byte>();
+        }
+        public void Update(string[] words)
+        {
+            foreach (System.Collections.Generic.KeyValuePair<string, byte> word in wordsInContext)
+            {
+                if (word.Value == 1)
+                {
+                    wordsInContext.Remove(word.Key);
+                }
+                else
+                {
+                    wordsInContext[word.Key] -= 1;
+                }
+            }
+            foreach (string word in words)
+            {
+                if (wordsInContext.ContainsKey(word))
+                {
+                    wordsInContext[word] += 10;
+                }
+                else
+                {
+                    wordsInContext.Add(word, 10);
+                }
+            }
         }
     }
 }
