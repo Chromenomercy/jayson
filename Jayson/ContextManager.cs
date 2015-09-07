59deaadd9f9e99bf8397ebb110b6476a674d7af7
Chromadude <PeterSearby8@googlemail.com>
2015-09-07 20:49:11 +0100

    empty contextmanager class

---
+++ Jayson/ContextManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jayson
+{
+    class ContextManager
+    {
+        public ContextManager()
+        {
+            Dictionary<string, byte> wordsInContext = new Dictionary<string, byte>();
+        }
+    }
+}
